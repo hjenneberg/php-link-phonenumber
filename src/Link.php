4e68859939637ded546f6420d6a06c7978116b5a
Jens Henneberg <hens.jenneberg@gmail.com>
2019-02-20 23:05:19 +0100

    Update lib to use strategies

--- src/Link.php
+++ src/Link.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace HJenneberg\LinkPhoneNumber;
 
 use HJenneberg\LinkPhoneNumber\Exception\InvalidNumberFormat;
+use HJenneberg\LinkPhoneNumber\Strategy\StrategyInterface;
 
 /**
  * Class Link
@@ -12,12 +13,25 @@ use HJenneberg\LinkPhoneNumber\Exception\InvalidNumberFormat;
 class Link
 {
     /**
+     * @var StrategyInterface
+     */
+    private $strategy;
+
+    /**
+     * @param StrategyInterface $strategy
+     */
+    public function __construct(StrategyInterface $strategy)
+    {
+        $this->strategy = $strategy;
+    }
+
+    /**
      * @param string $number
      *
      * @return string
      * @throws InvalidNumberFormat
      */
-    public static function get(string $number): string
+    public function get(string $number): string
     {
         $number = preg_replace('#[^\d+]#', '', $number);
 
