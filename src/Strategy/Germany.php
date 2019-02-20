142f08995e707b0151fdae984ec012387f9c2fe3
Jens Henneberg <hens.jenneberg@gmail.com>
2019-02-20 23:04:36 +0100

    Introduce area strategies

---
+++ src/Strategy/Germany.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HJenneberg\LinkPhoneNumber\Strategy;
+
+/**
+ * Class Germany
+ */
+class Germany implements StrategyInterface
+{
+
+}
---
+++ src/Strategy/StrategyInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HJenneberg\LinkPhoneNumber\Strategy;
+
+/**
+ * Class StrategyInterface
+ */
+interface StrategyInterface
+{
+
+}
