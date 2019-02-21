1f62f55a6954d3f586b61bbaeea9c03b78d2ef33
Jens Henneberg <hens.jenneberg@gmail.com>
2019-02-21 10:50:14 +0100

    Extract strategy for german phone numbers

--- src/Strategy/StrategyInterface.php
+++ src/Strategy/StrategyInterface.php
@@ -4,24 +4,34 @@ declare(strict_types=1);
 
 namespace HJenneberg\LinkPhoneNumber\Strategy;
 
-use HJenneberg\LinkPhoneNumber\Exception\InvalidNumberFormat;
-
 /**
  * Class StrategyInterface
  */
 interface StrategyInterface
 {
     /**
+     * Check if the given number is in a valid format for that area
+     *
+     * @param string $number
+     *
+     * @return bool
+     */
+    public function isValid(string $number): bool;
+
+    /**
+     * Remove all characters that do not belong to a valid phone number
+     *
+     * E.g. spaces, paranthesis, dashes
+     *
      * @param string $number
      *
      * @return string
      */
     public function cleanUp(string $number): string;
+
     /**
      * @param string $number
-     *
-     * @return void
-     * @throws InvalidNumberFormat
+     * @return string
      */
-    public function checkValidity(string $number): void;
+    public function transform(string $number): string;
 }
